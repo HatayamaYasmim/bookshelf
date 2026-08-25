4476595173748c891f22515dbd7fa132277c4f26
Yasmim Hatayama <yasmimh.hatayama@gmail.com>
2026-08-24 21:50:09 -0300

    feat: style book list with Spartan UI

--- frontend/src/app/features/books/book-list/book-list.component.ts
+++ frontend/src/app/features/books/book-list/book-list.component.ts
@@ -2,10 +2,17 @@ import { Component, inject, OnInit } from '@angular/core';
 
 import { BookService } from '../../../core/services/book.service';
 import { Book } from '../../../models/book.model';
+import { HlmTableImports } from '@spartan-ng/helm/table';
+import { HlmBadgeImports } from '@spartan-ng/helm/badge';
+import { HlmButtonImports } from '@spartan-ng/helm/button';
 
 @Component({
   selector: 'app-book-list',
-  imports: [],
+  imports: [
+    HlmTableImports,
+    HlmBadgeImports,
+    HlmButtonImports,
+  ],
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.scss',
 })

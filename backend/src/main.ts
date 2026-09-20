3a83b4a363b876917fe1cfd33ad399e7e18f4bc2
Yasmim Hatayama <yasmimh.hatayama@gmail.com>
2026-09-19 23:04:16 -0300

    chore: prepare backend for render deployment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,8 +6,10 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
-    credentials: true
+   origin:
+    process.env.FRONTEND_URL ??
+    'http://localhost:5173',
+    credentials: true,
   });
   app.useGlobalPipes(
     new ValidationPipe({
@@ -17,6 +19,10 @@ async function bootstrap() {
     }),
   );
   app.use(cookieParser());
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT ?? 3000;
+  await app.listen(
+    port,
+    '0.0.0.0',
+  );
 }
 bootstrap();
